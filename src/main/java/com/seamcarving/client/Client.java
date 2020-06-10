4931d143c7b2854024688bb77cbd3bf28315ddb8
Sachin Bhargava <sachin20@uw.edu>
2020-06-10 15:02:35 -0700

    initialized project

---
+++ src/main/java/com/seamcarving/client/Client.java
@@ -0,0 +1,8 @@
+package com.seamcarving.client;
+
+public class Client {
+    public static void main(String[] args) {
+        String path = "";
+
+    }
+}
---
+++ src/test/java/com/seamcarving/server/SeamCarvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamcarving.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamCarvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
