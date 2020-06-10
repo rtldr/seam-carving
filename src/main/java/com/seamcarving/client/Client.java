1c43120131c163b7cd54f72d7be8f25ea341b4a9
Sachin Bhargava <sachin20@uw.edu>
2020-06-10 16:02:07 -0700

    added client code and tested Picture.java dependency

--- src/main/java/com/seamcarving/client/Client.java
+++ src/main/java/com/seamcarving/client/Client.java
@@ -1,8 +1,11 @@
 package com.seamcarving.client;
 
+import com.seamcarving.lib.Picture;
+
 public class Client {
     public static void main(String[] args) {
-        String path = "";
-
+        String path = "images/christmas_original.jpg";
+        Picture pic = new Picture(path);
+        pic.show();
     }
 }
