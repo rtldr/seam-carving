7734b9620002e289f847b413582e840d6eee3c8e
Sachin Bhargava <sachin20@uw.edu>
2020-06-11 18:20:48 -0700

    added file-to-bytes and bytes-to-pic workflow

--- src/main/java/com/seamcarving/client/Client.java
+++ src/main/java/com/seamcarving/client/Client.java
@@ -2,10 +2,30 @@ package com.seamcarving.client;
 
 import com.seamcarving.lib.Picture;
 
+import java.io.*;
+
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String path = "images/christmas_original.jpg";
-        Picture pic = new Picture(path);
-        pic.show();
+        byte[] bytes = getBytesFromFilePath(path);
+        Picture picture = getPictureFromByteArray(bytes);
+        picture.show();
+    }
+
+    public static byte[] getBytesFromFilePath(String path) throws IOException {
+        File file = new File(path);
+        byte[] bytes = new byte[(int) file.length()];
+        FileInputStream fileInputStream = new FileInputStream(file);
+        fileInputStream.read(bytes);
+        fileInputStream.close();
+        return bytes;
+    }
+
+    public static Picture getPictureFromByteArray(byte[] bytes) throws IOException {
+        File tempFile = File.createTempFile("temp", ".tmp");
+        tempFile.deleteOnExit();
+        FileOutputStream fileOutputStream = new FileOutputStream(tempFile);
+        fileOutputStream.write(bytes);
+        return new Picture(tempFile);
     }
 }
